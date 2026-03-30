2b862d35452aea4ef62b3aa5d87f89ad514e17b1
Felipe Augusto <augusto.felipedev@gmail.com>
2026-03-30 13:05:42 -0300

    feat: add CI workflows and Form Tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import InfoSection from "../components/InfoSection";
-import OrderForm from "../components/OrderForm";
-import NossasDelicias from "../components/NossasDelicias"; // NOVO
-import Sabores from "../components/Sabores";
-import Testimonials from "../components/Testimonials"; // NOVO
-import Location from "../components/Location";
-import Footer from "../components/Footer";
+import Navbar from "../components/NavBar/Navbar";
+import Hero from "../components/Hero/Hero";
+import InfoSection from "../components/InfoSection/InfoSection";
+import OrderForm from "../components/OrderForm/OrderForm";
+import NossasDelicias from "../components/NossasDelicias/NossasDelicias"; // NOVO
+import Sabores from "../components/Sabores/Sabores";
+import Testimonials from "../components/Testimonials/Testimonials"; // NOVO
+import Location from "../components/Location/Location";
+import Footer from "../components/Footer/Footer";
 
 export default function Home() {
   return (
